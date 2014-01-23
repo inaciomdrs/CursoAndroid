0c899cb99ff668cf34922dfc40212e63eeca5392
Jonathas Melo <jonathas_ruam@hotmail.com>
2014-01-23 16:08:41 -0300

    Up em telas

--- SistemaMobile/gen/br/ufrn/example/sistemamobile/R.java
+++ SistemaMobile/gen/br/ufrn/example/sistemamobile/R.java
@@ -21,10 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_tlini=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,6 +37,8 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Cadatrar_OS=0x7f050005;
+        public static final int Visualizar_OS=0x7f050004;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050003;
