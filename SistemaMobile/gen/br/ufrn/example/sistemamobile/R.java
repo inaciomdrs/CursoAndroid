5f204e9060715edce2b235a88e3fbb07b9ebcea9
Jonathas Melo <jonathas_ruam@hotmail.com>
2014-01-24 14:14:42 -0300

    View Principal,cadastro e busca

--- SistemaMobile/gen/br/ufrn/example/sistemamobile/R.java
+++ SistemaMobile/gen/br/ufrn/example/sistemamobile/R.java
@@ -21,27 +21,42 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_tlini=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
+        public static final int ic_tlini=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080001;
+        public static final int imageView1=0x7f080003;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_busca=0x7f030000;
+        public static final int activity_cadastro=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int busca=0x7f070000;
+        public static final int cadastro=0x7f070001;
+        public static final int main=0x7f070002;
     }
     public static final class string {
+        public static final int Cad_OS_END_FINAL=0x7f05000a;
+        public static final int Cad_OS_END_START=0x7f05000b;
+        public static final int Cad_OS_NUMBER=0x7f05000e;
+        public static final int Cad_OS_NameClient=0x7f050009;
+        public static final int Cad_OS_OBS=0x7f05000c;
+        public static final int Cad_OS_STATUS=0x7f05000d;
         public static final int Cadatrar_OS=0x7f050005;
+        public static final int Ic_main=0x7f05000f;
         public static final int Visualizar_OS=0x7f050004;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050003;
+        public static final int texto_busca=0x7f050007;
+        public static final int title_activity_busca=0x7f050006;
+        public static final int title_activity_cadastro=0x7f050008;
         public static final int title_activity_main=0x7f050001;
     }
     public static final class style {
--- SistemaMobile/src/br/ufrn/sistemamobile/MainActivity.java
+++ SistemaMobile/src/br/ufrn/sistemamobile/MainActivity.java
@@ -1,16 +1,23 @@
 package br.ufrn.sistemamobile;
 
+import br.ufrn.example.sistemamobile.BuscaActivity;
+import br.ufrn.example.sistemamobile.CadastroActivity;
 import br.ufrn.example.sistemamobile.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		
 	}
 
 	@Override
@@ -18,6 +25,16 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void abrirOS(View v){
+		Intent i = new Intent(this,CadastroActivity.class);
+		startActivity(i);
+	}
+	
+	public void buscaOS(View v){
+		Intent i = new Intent(this,BuscaActivity.class);
+		startActivity(i);
 	}
 
 }
